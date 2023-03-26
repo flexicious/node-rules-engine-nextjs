1beef6e17df76a9c0b8ee5f1883d78429e511b61
flexicious <flexicious@gmail.com>
2023-03-25 22:13:00 -0400

    api key

--- shared/config-utils.ts
+++ shared/config-utils.ts
@@ -44,3 +44,47 @@ export const loadConfigApi = async (lambdaName:string):Promise<ConfigApi> => {
     );
     return configApi;
 };
+
+//For enterprise customers with an API key, you can uncomment the code below to load the config from the server
+
+// const apiKey = "YOUR_API_KEY";
+// const environment = "dev";
+// const headers =  {
+//     "x-api-key": apiKey,
+//     "environment": environment
+// };
+// export const loadConfigApi = async (lambdaName:string):Promise<ConfigApi> => {
+//     const configApi = await getNodeJsConfigApi(
+//         {
+//             lambdaName,
+//             cacheDurationSeconds: 60,
+//             loadConfig: async (lastRefreshed?:Date, existingConfig?:ConfigJson) => {
+//                 // Once you have an API key, you can uncomment this code to load the config from the server
+//                 if(lastRefreshed && existingConfig){
+//                     const configLastUpdated = await fetch("https://config.lambdagenie.com/lastUpdated", {
+//                         method: "POST",
+//                         headers
+//                     }).then((res) => res.json()).then((json) => new Date(json.updatedAt));
+//                     console.log("Last Refreshed", lastRefreshed, "Config Last Updated", configLastUpdated);
+//                     if(lastRefreshed && configLastUpdated && lastRefreshed.getTime() >= configLastUpdated.getTime()){
+//                         console.log("Config is up to date");
+//                         return existingConfig;  
+//                     }
+//                 }
+//                 console.log("Loading Config");
+//                 const file = await fetch("https://config.lambdagenie.com/config", {
+//                     method: "POST",
+//                     headers
+//                 }).then((res) => res.json());
+//                 const configJson = file;
+//                 console.log("Config Json", { configJson });
+//                 return configJson as unknown as ConfigJson;
+
+//             },
+//             log: (level, message, extra) => {
+//                 console.log(level, message, extra);
+//             }
+//         }
+//     );
+//     return configApi;
+// };
